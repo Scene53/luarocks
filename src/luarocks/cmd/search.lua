1af67aa7ec94f475d12a16353ae8d8247aeff95b
Bar Perez <bar@scene53.com>
2019-07-30 12:22:50 +0300

    no message

--- src/luarocks/cmd/search.lua
+++ src/luarocks/cmd/search.lua
@@ -21,6 +21,8 @@ cmd_search.help = [[
 --porcelain  Return a machine readable format.
 ]]
 
+local function is_empty_table(tbl) return next(tbl) == nil end
+
 --- Splits a list of search results into two lists, one for "source" results
 -- to be used with the "build" command, and one for "binary" results to be
 -- used with the "install" command.
@@ -28,20 +30,20 @@ cmd_search.help = [[
 -- @return (table, table): Two tables, one for source and one for binary
 -- results.
 local function split_source_and_binary_results(result_tree)
-   local sources, binaries = {}, {}
-   for name, versions in pairs(result_tree) do
-      for version, repositories in pairs(versions) do
-         for _, repo in ipairs(repositories) do
-            local where = sources
-            if repo.arch == "all" or repo.arch == cfg.arch then
-               where = binaries
+    local sources, binaries = {}, {}
+    for name, versions in pairs(result_tree) do
+        for version, repositories in pairs(versions) do
+            for _, repo in ipairs(repositories) do
+                local where = sources
+                if repo.arch == "all" or repo.arch == cfg.arch then
+                    where = binaries
+                end
+                local entry = results.new(name, version, repo.repo, repo.arch)
+                search.store_result(where, entry)
             end
-            local entry = results.new(name, version, repo.repo, repo.arch)
-            search.store_result(where, entry)
-         end
-      end
-   end
-   return sources, binaries
+        end
+    end
+    return sources, binaries
 end
 
 --- Driver function for "search" command.
@@ -51,31 +53,36 @@ end
 -- error message otherwise.
 function cmd_search.command(flags, name, version)
 
-   name = util.adjust_name_and_namespace(name, flags)
+    name = util.adjust_name_and_namespace(name, flags)
 
-   if flags["all"] then
-      name, version = "", nil
-   end
+    if flags["all"] then
+        name, version = "", nil
+    end
 
-   if type(name) ~= "string" and not flags["all"] then
-      return nil, "Enter name and version or use --all. "..util.see_help("search")
-   end
-   
-   local query = queries.new(name:lower(), version, true)
-   local result_tree, err = search.search_repos(query)
-   local porcelain = flags["porcelain"]
-   local full_name = name .. (version and " " .. version or "")
-   util.title(full_name .. " - Search results for Lua "..cfg.lua_version..":", porcelain, "=")
-   local sources, binaries = split_source_and_binary_results(result_tree)
-   if next(sources) and not flags["binary"] then
-      util.title("Rockspecs and source rocks:", porcelain)
-      search.print_result_tree(sources, porcelain)
-   end
-   if next(binaries) and not flags["source"] then    
-      util.title("Binary and pure-Lua rocks:", porcelain)
-      search.print_result_tree(binaries, porcelain)
-   end
-   return true
+    if type(name) ~= "string" and not flags["all"] then
+        return nil, "Enter name and version or use --all. "..util.see_help("search")
+    end
+
+    local query = queries.new(name:lower(), version, true)
+    local result_tree, err = search.search_repos(query)
+    local porcelain = flags["porcelain"]
+    local full_name = name .. (version and " " .. version or "")
+    util.title(full_name .. " - Search results for Lua "..cfg.lua_version..":", porcelain, "=")
+    local sources, binaries = split_source_and_binary_results(result_tree)
+    if next(sources) and not flags["binary"] then
+        util.title("Rockspecs and source rocks:", porcelain)
+        search.print_result_tree(sources, porcelain)
+    end
+    if next(binaries) and not flags["source"] then    
+        util.title("Binary and pure-Lua rocks:", porcelain)
+        search.print_result_tree(binaries, porcelain)
+    end
+    if is_empty_table(result_tree) then
+        print("couldn't find searched rock: " .. name)
+        return false
+    else
+        return true
+    end
 end
 
 return cmd_search
